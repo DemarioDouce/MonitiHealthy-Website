c84703d9594f78bfed149c0490e2d224c4060bf2
DemarioDouce <demario.douce567@gmail.com>
2021-03-04 21:20:21 -0500

    Improved Nav UI.

--- frontend/src/res/components/NavComponent.js
+++ frontend/src/res/components/NavComponent.js
@@ -1,24 +1,51 @@
-//Load components
-import {
-  Navbar,
-  Nav,
-  NavDropdown,
-  Form,
-  FormControl,
-  Button,
-} from "react-bootstrap";
+//Load npm module react-bootstrap.
+import { Container, Navbar, Nav, Button } from "react-bootstrap";
+//Load favicon image.
+import favicon from "../img/Favicon.png";
 const NavComponent = () => {
   return (
-    <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-        </Nav>
-        <Button variant="primary">Search</Button>
-      </Navbar.Collapse>
+    <Navbar expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img
+            src={favicon}
+            width="60"
+            height="60"
+            className="d-inline-block align-top"
+            alt="MonitiHealthy favicon icon"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse
+          className="justify-content-center"
+          id="basic-navbar-nav"
+        >
+          <Nav className="mr-auto">
+            <Nav.Link></Nav.Link>
+            <Nav.Link></Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        <Button
+          style={{ marginLeft: "5px", marginRight: "5px" }}
+          className="justify-content-end"
+          variant="primary"
+        >
+          LOGIN
+        </Button>
+        <Button
+          style={{
+            marginLeft: "5px",
+            marginRight: "5px",
+            background: "#fff",
+            color: "#0d6efd",
+            border: "2px solid #0d6efd",
+          }}
+          className="justify-content-end"
+          variant="dark"
+        >
+          SIGNUP
+        </Button>
+      </Container>
     </Navbar>
   );
 };
