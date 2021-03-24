264fc1a70024dff793c6e1aa032450d61b52f322
Kenneth General <kennethlorenzg@gmail.com>
2021-03-24 01:18:56 -0400

    add patient and nurse model

---
+++ app/models/nurse.server.model.js
@@ -0,0 +1,47 @@
+﻿const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+const NurseSchema = new Schema({
+  firstName: { type: String, required: true },
+
+  lastName: { type: String, required: true },
+  employeeNumber: {
+    type: Number,
+    // Set a unique 'student number' index
+    unique: true,
+    // Validate 'username' value existance
+    required: "Employee Number is required",
+  },
+  phoneNumber: { type: Number, required: true },
+
+  address: { type: String, required: true },
+
+  city: { type: String, required: true },
+
+  password: {
+    type: String,
+    // Validate the 'password' value length
+    validate: [
+      (password) => password && password.length > 6,
+      "Password should be longer",
+    ],
+  },
+});
+
+// Use a pre-save middleware to hash the password
+// before saving it into database
+NurseSchema.pre("save", function (next) {
+  //hash the password before saving it
+  this.password = bcrypt.hashSync(this.password, saltRounds);
+  next();
+});
+
+// Create an instance method for authenticating user
+NurseSchema.methods.authenticate = function (password) {
+  //compare the hashed password of the database
+  //with the hashed version of the password the user enters
+  return this.password === bcrypt.hashSync(password, saltRounds);
+};
+mongoose.model("Nurse", NurseSchema);
