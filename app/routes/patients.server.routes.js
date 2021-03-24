f43b6a30a1d6b9f6e7d43fc95087e391f324def0
Kenneth General <kennethlorenzg@gmail.com>
2021-03-24 02:02:49 -0400

    Added patient and nurse routes

---
+++ app/routes/patients.server.routes.js
@@ -0,0 +1,11 @@
+﻿// Load the 'users' controller
+var patients = require("../../app/controllers/patients.server.controller");
+// Define the routes module' method
+module.exports = function (app) {
+  //handle a post request made to root path
+  app.post("/registerpatient", patients.create);
+  //authenticate user
+  app.post("/signinpatient", patients.authenticate);
+  app.get("/signoutpatient", patients.signout);
+  app.get("/read_cookie", patients.isSignedIn);
+};
