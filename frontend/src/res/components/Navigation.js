90c70f8dae7c2bfa15c239b8d0a1eafdd6cae8c5
Kenneth General <kennethlorenzg@gmail.com>
2021-03-27 06:56:19 -0400

    Created Navbar and Post Signup page

---
+++ frontend/src/res/components/Navigation.js
@@ -0,0 +1,67 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Button from "@material-ui/core/Button";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { Link as ReactLink } from "react-router-dom";
+import Link from "@material-ui/core/Link";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  "@global": {
+    ul: {
+      margin: 0,
+      padding: 0,
+      listStyle: "none",
+    },
+  },
+  appBar: {
+    borderBottom: `1px solid ${theme.palette.divider}`,
+  },
+  toolbar: {
+    flexWrap: "wrap",
+  },
+  toolbarTitle: {
+    flexGrow: 1,
+  },
+  link: {
+    margin: theme.spacing(1, 1.5),
+  },
+}));
+
+function Navigation() {
+  const classes = useStyles();
+
+  return (
+    <AppBar
+      position="static"
+      color="default"
+      elevation={0}
+      className={classes.appBar}
+    >
+      <Toolbar className={classes.toolbar}>
+        <Typography
+          variant="h6"
+          color="inherit"
+          noWrap
+          className={classes.toolbarTitle}
+        >
+          MonitiHealthy
+        </Typography>
+        <nav>
+          <Link
+            component={ReactLink}
+            to="/"
+            variant="button"
+            color="textPrimary"
+            className={classes.link}
+          >
+            Home
+          </Link>
+        </nav>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default Navigation;
