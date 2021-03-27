6595f4f3528a24da358696b3541e42875107f543
Kenneth General <kennethlorenzg@gmail.com>
2021-03-27 00:27:21 -0400

    Update Copyright.js

--- frontend/src/res/components/Copyright.js
+++ frontend/src/res/components/Copyright.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+//this functional component is to be used in Forms such as sign in / signup page
+
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
