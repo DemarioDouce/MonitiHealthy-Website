bde8b9bec91daf30620d7dba494c118b3334ca93
Kenneth General <kennethlorenzg@gmail.com>
2021-04-12 18:17:41 -0400

    Added api for creating vital sign in the nurse page

--- app/routes/healthInfos.server.routes.js
+++ app/routes/healthInfos.server.routes.js
@@ -1,8 +1,17 @@
-const patients = require('../../app/controllers/patients.server.controller');
-const healthInfo = require('../../app/controllers/healthinfo.server.controller');
+const patients = require("../../app/controllers/patients.server.controller");
+const healthInfo = require("../../app/controllers/healthinfo.server.controller");
+const nurses = require("../../app/controllers/nurses.server.controller");
 //
 module.exports = function (app) {
-        app.route('/api/add-healthInfo')
-               .post(patients.requiresLogin, healthInfo.create);
-        app.get('/api/all-healthinfo',patients.requiresLogin, healthInfo.healthinfobyPatient);
-}
+  app
+    .route("/api/add-healthInfo")
+    .post(patients.requiresLogin, healthInfo.create);
+  app.get(
+    "/api/all-healthinfo",
+    patients.requiresLogin,
+    healthInfo.healthinfobyPatient
+  );
+  app
+    .route("/api/nurse-add-healthInfo")
+    .post(nurses.requiresLogin, healthInfo.create);
+};
