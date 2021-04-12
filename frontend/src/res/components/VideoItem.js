b485d361bf495706cfd1623862cf031a1f7ac6eb
Kenneth General <kennethlorenzg@gmail.com>
2021-04-12 12:14:14 -0400

    update video items

--- frontend/src/res/components/VideoItem.js
+++ frontend/src/res/components/VideoItem.js
@@ -5,7 +5,7 @@ const VideoItem = ({ video, onVideoSelect }) => {
   //whenever someone clicks the component, we want to call the onVideoSelect method
   //and pass the video as an argument to it.
   return (
-    <div onClick={onVideoSelect(video)} className="video-item item">
+    <div onClick={() => onVideoSelect(video)} className="video-item item">
       <img
         alt={video.snippet.title}
         className="ui image"
