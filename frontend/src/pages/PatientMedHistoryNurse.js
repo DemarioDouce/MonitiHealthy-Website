63eadf62a5cc81a0cf8697b6dd151ebd36dcea37
Kenneth General <kennethlorenzg@gmail.com>
2021-04-12 20:45:02 -0400

    Added patients fullname to the view, and date to the info

--- frontend/src/pages/PatientMedHistoryNurse.js
+++ frontend/src/pages/PatientMedHistoryNurse.js
@@ -10,36 +10,40 @@ import FooterComponent from "../res/components/FooterComponent";
 function PatientMedHistoryNurse(props) {
   const [info, setInfo] = useState();
   const [healthHistory, setHealthHistory] = useState([]);
-  console.log(props.location.state.detail)
+  console.log(props.location.state.detail);
+  const fullName = props.location.state.fullName;
 
   //setInfo(props.location.state.detail);
-  
+
   useEffect(() => {
     setInfo(props.location.state.detail);
-    const apiUrl = "http://localhost:3000/api/patientrecs/"+props.location.state.detail;
+    const apiUrl =
+      "http://localhost:3000/api/patientrecs/" + props.location.state.detail;
     const getPatientHistory = async () => {
       console.log("--- in getPatientHistory function ---");
-      await axios.get(apiUrl)
-        .then(result => {
+      await axios
+        .get(apiUrl)
+        .then((result) => {
           //console.log('result.data:',result.data)
           //check if the user has logged in
           //if(result.data.screen !== 'auth')
           //{
-            
-            //console.log('data in if:', result.data )
-            //setData(result.data);
-            //setRowData(result.data);
-            console.log("--- in getPatientHistoryAxios function ---");
-            setHealthHistory(result.data)
-            console.log(result.data)
-            
+
+          //console.log('data in if:', result.data )
+          //setData(result.data);
+          //setRowData(result.data);
+          console.log("--- in getPatientHistoryAxios function ---");
+          setHealthHistory(result.data);
+          console.log(result.data);
+
           //}
-        }).catch((error) => {
-          console.log('error in fetchData:', error)
+        })
+        .catch((error) => {
+          console.log("error in fetchData:", error);
         });
-        //rowsArray(data);
-      };  
-      getPatientHistory();
+      //rowsArray(data);
+    };
+    getPatientHistory();
   }, []);
   return (
     <>
@@ -54,11 +58,16 @@ function PatientMedHistoryNurse(props) {
               right: "0",
               margin: "auto",
               width: "50vw",
-              height: "50vh",
+              height: "90vh",
             }}
           >
+            <h3>{fullName}'s Medical History</h3>
+            <Link to="/nurse-dashboard" style={{ textDecoration: "none" }}>
+              <p style={{ margin: "20px" }}>Go Back</p>
+            </Link>
             <ListGroup>
-                {healthHistory.map((healthinfo) => (<ListGroup.Item
+              {healthHistory.map((healthinfo) => (
+                <ListGroup.Item
                   style={{
                     width: "100%",
                     padding: "12px 20px",
@@ -69,17 +78,21 @@ function PatientMedHistoryNurse(props) {
                     boxSizing: "border-box",
                   }}
                 >
-                  Pulse Rate: {healthinfo.pulseRate}<br></br>
-                  Weight: {healthinfo.weight}<br></br>
-                  Temperature: {healthinfo.temperature}<br></br>
-                  Blood Pressure: {healthinfo.bloodPressure}<br></br>
-                  Respiratory Rate: {healthinfo.respiratoryRate}<br></br>
-                </ListGroup.Item>))}
-                
-              </ListGroup>
-            <Link to="/nurse-dashboard" style={{ textDecoration: "none" }}>
-              <p style={{ margin: "20px" }}>Go Back</p>
-            </Link>
+                  Date: {healthinfo.date}
+                  <br></br>
+                  Pulse Rate: {healthinfo.pulseRate}
+                  <br></br>
+                  Weight: {healthinfo.weight}
+                  <br></br>
+                  Temperature: {healthinfo.temperature}
+                  <br></br>
+                  Blood Pressure: {healthinfo.bloodPressure}
+                  <br></br>
+                  Respiratory Rate: {healthinfo.respiratoryRate}
+                  <br></br>
+                </ListGroup.Item>
+              ))}
+            </ListGroup>
             <FooterComponent color="black" />
           </div>
         </div>
