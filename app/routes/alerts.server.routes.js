0830b7c556da7308b3b8971fb67f40b89acfc5ef
DemarioDouce <demario.douce567@gmail.com>
2021-04-12 21:49:35 -0400

    Removed.

--- app/routes/alerts.server.routes.js
+++
@@ -1,8 +0,0 @@
-const patients = require('../../app/controllers/patients.server.controller');
-const alert = require('../../app/controllers/alerts.server.controller');
-//
-module.exports = function (app) {
-        app.route('/api/send-alert')
-               .post(patients.requiresLogin, alert.create);
-        app.get('/api/alerts',patients.requiresLogin, alert.alertsbyPatient);
-}
--- config/env/development.js
+++
@@ -1,7 +0,0 @@
-﻿//Development configuration options
-//To sign the session identifier, use a secret string
-module.exports = {
-  db: "mongodb://localhost/MonitiHealthy",
-  sessionSecret: "developmentSessionSecret",
-  secretKey: "real_secret",
-};
