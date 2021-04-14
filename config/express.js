af16530c1c200c11697daa338e935fdb38fa8ed9
Kenneth General <kennethlorenzg@gmail.com>
2021-04-14 01:48:37 -0400

    Update express.js

--- config/express.js
+++ config/express.js
@@ -67,7 +67,6 @@ module.exports = function () {
   require("../app/routes/patients.server.routes.js")(app);
   require("../app/routes/healthInfos.server.routes.js")(app);
   require("../app/routes/alerts.server.routes.js")(app);
-  require("../app/routes/motivationaltips.server.routes.js")(app);
   //The express.static() middleware takes one argument
   //to determine the location of the static folder
   //Configure static file serving
