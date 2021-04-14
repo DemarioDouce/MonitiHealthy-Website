ef697a09b2e4d9cd30f2fad0b2b0c60b5b552db3
Kenneth General <kennethlorenzg@gmail.com>
2021-04-14 01:49:42 -0400

    Delete motivationaltips.model.js

--- app/models/motivationaltips.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-var MotivationalTips = new Schema({
-  message: { type: String, required: "Motivational tip is required" },
-  patient: [{ type: Schema.ObjectId, ref: "Patient" }],
-});
-
-mongoose.model("MotivationalTips", MotivationalTips);
