0d453d7b263e4f617ce869a6a9be8a587dd49e8b
Kenneth General <kennethlorenzg@gmail.com>
2021-04-14 01:49:33 -0400

    added api post request to send daily tips

--- app/routes/nurses.server.routes.js
+++ app/routes/nurses.server.routes.js
@@ -1,5 +1,5 @@
 ﻿const nurses = require("../../app/controllers/nurses.server.controller");
-var patients = require("../../app/controllers/patients.server.controller");
+const patients = require("../../app/controllers/patients.server.controller");
 //
 module.exports = function (app) {
   //handle a post request made to root path
@@ -10,9 +10,12 @@ module.exports = function (app) {
   app.get("/read_cookie", nurses.isSignedIn);
   app.get("/all_patients", patients.list);
 
-  app.route('/api/patientrecs/:patientId')
-            .get(patients.healthinfobyPatient)
-            .post(nurses.addPatientHealthInfo)
+  app
+    .route("/api/patientrecs/:patientId")
+    .get(patients.healthinfobyPatient)
+    .post(nurses.addPatientHealthInfo);
 
-  app.param('patientId', nurses.patientByID);
+  app.post("/send-daily-tips/:patientId", nurses.sendMotivationalTips);
+
+  app.param("patientId", nurses.patientByID);
 };
