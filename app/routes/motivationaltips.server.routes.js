eada75a9b2d6a7144043236efdcd0de00f98d7ec
Kenneth General <kennethlorenzg@gmail.com>
2021-04-14 01:49:39 -0400

    Delete motivationaltips.server.routes.js

--- app/routes/motivationaltips.server.routes.js
+++
@@ -1,7 +0,0 @@
-const patients = require('../../app/controllers/patients.server.controller');
-const tips = require('../../app/controllers/motivationaltips.server.controller');
-//
-module.exports = function (app) {
-        app.route('/api/send-tips')
-               .post(patients.requiresLogin, tips.create);
-}
